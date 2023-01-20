6dc9ad12b3f8d6927a26299378304790cf9a676f
Hany Andriyanto <hany.andriyanto@gmail.com>
2023-01-20 13:54:02 +0700

    Add config data and get token function

---
+++ confdata.js
@@ -0,0 +1,25 @@
+export const ws_address = 'ws://180.250.145.106:8000/connection/websocket';
+export default { ws_address: ws_address, ws_token: ws_token }
+
+function getToken(url, ctx) {
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: 'POST',
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+      body: JSON.stringify(ctx)
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(`Unexpected status code ${res.status}`);
+        }
+        return res.json();
+      })
+      .then(data => {
+        resolve(data.token);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+}
+
